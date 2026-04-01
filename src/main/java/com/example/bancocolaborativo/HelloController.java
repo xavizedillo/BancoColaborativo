81a8489af6fd42c5b22d304a36c83f566ed30d4e
xavizedillo <20253rd003@utez.edu.mx>
2026-04-01 15:06:37 -0600

    Programe el hello controller

--- src/main/java/com/example/bancocolaborativo/HelloController.java
+++ src/main/java/com/example/bancocolaborativo/HelloController.java
@@ -1,14 +1,56 @@
 package com.example.bancocolaborativo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private TextField txtPass;
+    @FXML
+    private Label lblError;
+
+    @FXML
+    private void login(ActionEvent event) throws IOException {
+        if(txtUser.getText().equals("Jeringas") && txtpass.getText().equals("tacogalleta123")){
+            Parent root = FXMLLoader.load(getClass().getResource("loginSuccess.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }else {
+            lblError.setVisible(true);
+        }
+    }
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
